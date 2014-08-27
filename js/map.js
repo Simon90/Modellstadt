ccfabfe8214c963e818a6615e62e038fc37e923b
Nikolai Gorte <n.gorte@gmail.com>
2014-08-27 12:28:54 +0200

    Relative map size

--- js/map.js
+++ js/map.js
@@ -4,3 +4,7 @@ var map = L.map('map').setView([51.9743713, 7.41444848], 18);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
 			}).addTo(map);
+			
+			
+$("#map").height($(window).height()*0.6);
+map.invalidateSize();
