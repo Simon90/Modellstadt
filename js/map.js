94e7fc8aed6d71751c0da5cc0dde995ad12634f2
Nikolai Gorte <n.gorte@gmail.com>
2014-08-28 22:49:51 +0200

    marker

--- js/map.js
+++ js/map.js
@@ -12,14 +12,8 @@ $("#map").height($(window).height()*0.6);
 map.invalidateSize();
 
 
-// No Scrollbar 
-		$('html, body').css({
-		'overflow': 'hidden',
-		'height': '100%'
-		})
-		
-		
-		
+	
+			
 
 // refresh data 
 // returns json object		
@@ -30,34 +24,49 @@ function refreshData()
       url: "mysql.php",     //the script to call to get data          
       data: "",             //you can insert url argumnets here 
       dataType: 'json',     //data format      
-      success: createTable  //success function
+      success: createContent  //success function
     });
   } 
 
-// Execute every 5 seconds
-window.setInterval(refreshData, 5000); 
+// Execute every 10 seconds
+window.setInterval(refreshData, 10000); 
   
 
-// create table content
-function createTable(data)  {
+// marker layer
+var markers = new L.layerGroup();
+
+// create table and map content
+function createContent(data)  {
+	map.removeLayer(markers); // clear map 
 	var type = "";
 	var value = "";
 	var unit = "";
-	var tbl_body = "";
+	var lat = "";
+	var lon = "";
+	var tbl_body = ""; //table body html string
 	$.each(data, function(i, item) {
-		var tbl_row = "";
+		var tbl_row = ""; //table row html string
 		type = (item.Type);
 		value = (item.Value);
 		unit = (item.Unit);
-		tbl_row = "<td>"+type+"</td><td>"+value+"</td><td>"+unit+"</td><td>";
-		tbl_body += "<tr>"+tbl_row+"</tr>";
+		lat = (item.lat)
+		lon = (item.lon)
+		createMarkers(lat, lon, type, value, unit);
+		tbl_row = "<td>"+type+"</td><td>"+value+"</td><td>"+unit+"</td><td>"; //fill row
+		tbl_body += "<tr>"+tbl_row+"</tr>"; // fill table body
 	});
+	markers.addTo(map); //add markers to map
 	$("#table1 tbody").html(tbl_body); //insert into body of table1
 }  
 
-		
 
 
+function createMarkers(lat, lon, type, value, unit)  {
+	var marker = new L.marker();
+	marker.setLatLng([lat, lon]);
+	marker.bindPopup(type +": "+value+" "+unit);
+	markers.addLayer(marker);
+}
 
 
 
